735fc735118b1b68664378f74f205362b10b68aa
Pangratios Cosma <pagcosma@gmail.com>
2020-04-20 10:26:01 +0300

    composer ide generation

--- Entities/ConditionCodeLookupCache.php
+++ Entities/ConditionCodeLookupCache.php
@@ -5,16 +5,29 @@ namespace subzero10\ConditionCodeLookup\Entities;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- *
  * Class ConditionCodeLookupCache
- * @package CircleLinkHealth\ConditionCodeLookup\Entities
  *
+ * @package CircleLinkHealth\ConditionCodeLookup\Entities
  * @property int $id
  * @property string $type
  * @property string $code
  * @property string $name
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
+ * @property string $request_url
+ * @property string $response_raw
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereRequestUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereResponseRaw($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\subzero10\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class ConditionCodeLookupCache extends Model
 {
