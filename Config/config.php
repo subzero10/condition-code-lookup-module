e6785da3666e3627c3d33261d83c9c9b2708d408
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2020-04-22 23:13:46 -0500

    Removes database info from config
    DB connection info should be in config/database.php, and only pass the driver to consuming code

--- Config/config.php
+++ Config/config.php
@@ -4,18 +4,6 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-use Illuminate\Support\Str;
-
-$mysqlDBName = env('CCL_DB_DATABASE', env('DB_DATABASE', 'nothing'));
-
-if ('nothing' === $mysqlDBName) {
-    $mysqlDBName = Str::snake(getenv('HEROKU_BRANCH'));
-}
-
-if (getenv('CI')) {
-    $mysqlDBName = getenv('HEROKU_TEST_RUN_ID');
-}
-
 return [
     'name'     => 'ConditionCodeLookup',
     'api_urls' => [
@@ -30,10 +18,5 @@ return [
     ],
     'cache' => [
         'enable'   => env('CCL_CACHE_ENABLE', false),
-        'host'     => env('CCL_DB_HOST', env('DB_HOST', '127.0.0.1')),
-        'port'     => env('CCL_DB_PORT', env('DB_PORT', '3306')),
-        'database' => $mysqlDBName,
-        'username' => env('CCL_DB_USERNAME', env('DB_USERNAME', 'forge')),
-        'password' => env('CCL_DB_PASSWORD', env('DB_PASSWORD', '')),
     ],
 ];
