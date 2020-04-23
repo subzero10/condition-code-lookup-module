37ec0dce0532185ae920fd74abc3de0f7d0451a3
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-22 21:05:10 -0500

    Only linting

--- Entities/ConditionCodeLookupCache.php
+++ Entities/ConditionCodeLookupCache.php
@@ -20,6 +20,17 @@ use Illuminate\Database\Eloquent\Model;
  * @property string         $request_url
  * @property string         $response_raw
  * @mixin \Eloquent
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereRequestUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereResponseRaw($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\ConditionCodeLookup\Entities\ConditionCodeLookupCache whereUpdatedAt($value)
  */
 class ConditionCodeLookupCache extends Model
 {
